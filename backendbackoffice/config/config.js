22e5e79ea22d6333350a9cd6d069168ca1b2d1ed
jihedgabsi <152264496+jihedgabsi@users.noreply.github.com>
2025-08-25 16:24:51 +0100

    Delete backendbackoffice directory

--- backendbackoffice/config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  PORT: process.env.PORT ,
-  MONGO_URI: process.env.MONGO_URI ,
-  IMAGE_SERVER_URL: process.env.IMAGE_SERVER_URL || 'http://localhost:3000/upload'
-};
--- backendbackoffice/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app');
-const config = require('./config/config');
-
-// Start the server
-app.listen(config.PORT, () => {
-  console.log(`Baggage Collection Server running on port ${config.PORT}`);
-});
